9bcdcec7a2a54268a3b0a0b9775f40d2ccbd2e21
RYuriy <radov.yuriy@ukr.net>
2021-12-01 01:41:37 +0200

    + convert logic

--- lib/main.dart
+++ lib/main.dart
@@ -10,10 +10,13 @@ class MyApp extends StatefulWidget {
 }
 
 class MyAppState extends State<MyApp> {
-  double? _numberForm;
-  String? _startMeasure;
-  String? _convertedMeasure;
+  double _numberForm = 0.0;
+  String _startMeasure = '';
+  String _convertedMeasure = '';
+  String _resultMessage = '';
+
   final List<String> _measures = [
+    '',
     'meters',
     'kilometers',
     'grams',
@@ -24,9 +27,34 @@ class MyAppState extends State<MyApp> {
     'ounces',
   ];
 
+  final Map<String, int> _measuresMap = {
+    'meters': 0,
+    'kilometers': 1,
+    'grams': 2,
+    'kilograms': 3,
+    'feet': 4,
+    'miles': 5,
+    'pounds (lbs)': 6,
+    'ounces': 7,
+  };
+
+  final dynamic _formulas = {
+    '0': [1, 0.001, 0, 0, 3.28084, 0.000621371, 0, 0],
+    '1': [1000, 1, 0, 0, 3280.84, 0.621371, 0, 0],
+    '2': [0, 0, 1, 0.0001, 0, 0, 0.00220462, 0.035274],
+    '3': [0, 0, 1000, 1, 0, 0, 2.20462, 35.274],
+    '4': [0.3048, 0.0003048, 0, 0, 1, 0.000189394, 0, 0],
+    '5': [1609.34, 1.60934, 0, 0, 5280, 1, 0, 0],
+    '6': [0, 0, 453.592, 0.453592, 0, 0, 1, 16],
+    '7': [0, 0, 28.3495, 0.0283495, 3.28084, 0, 0.0625, 1],
+  };
+
   @override
   void initState() {
     _numberForm = 0;
+    _startMeasure = _measures[0];
+    _convertedMeasure = _measures[0];
+    _resultMessage = '';
     super.initState();
   }
 
@@ -63,9 +91,8 @@ class MyAppState extends State<MyApp> {
                   ),
                   onChanged: (text) {
                     var rv = double.tryParse(text);
-                    //rv ??= 0;
                     setState(() {
-                      _numberForm = rv;
+                      _numberForm = rv ??= 0;
                     });
                   }),
               const Spacer(),
@@ -120,12 +147,20 @@ class MyAppState extends State<MyApp> {
               ),
               ElevatedButton(
                 child: Text("Convert", style: inputStyle),
-                onPressed: () => true,
+                onPressed: () {
+                  if (_startMeasure.isEmpty ||
+                      _convertedMeasure.isEmpty ||
+                      _numberForm == 0) {
+                    return;
+                  } else {
+                    convert(_numberForm, _startMeasure, _convertedMeasure);
+                  }
+                },
               ),
               const Spacer(
                 flex: 2,
               ),
-              Text((_numberForm == null) ? '' : _numberForm.toString(),
+              Text(_resultMessage == '' ? '' : _resultMessage,
                   style: labelStyle),
               const Spacer(
                 flex: 8,
@@ -136,4 +171,21 @@ class MyAppState extends State<MyApp> {
       ),
     );
   }
+
+  void convert(double value, String from, String to) {
+    int nFrom = _measuresMap[from] ?? 0;
+    int nTo = _measuresMap[to] ?? -1;
+    var multiplier = _formulas[nFrom.toString()][nTo];
+    double result = value * multiplier;
+    if (result == 0) {
+      _resultMessage = 'This conversion cannot be performed';
+    } else {
+      _resultMessage =
+          '${_numberForm.toString()} $_startMeasure are ${result.toString()} $_convertedMeasure';
+    }
+
+    setState(() {
+      _resultMessage = _resultMessage;
+    });
+  } // convert
 }
